35af007bcf67fff881eee74c07e3308b0a8df590
Burak torman <buraktorman@Buraks-MBP.home>
2023-06-28 23:29:07 -0400

    Added symbol pallete

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './style/App.css';
+import { useState } from 'react';
+import {
+  Diagram,
+  DiagramComponent,
+  Inject,
+  NodeModel,
+  BpmnShape,
+  BpmnSubProcessModel,
+  BpmnDiagrams,
+  BpmnActivityModel,
+  BpmnFlowModel,
+  SnapConstraints,
+  Snapping,
+  ConnectorEditing,
+  LineRouting
+} from "@syncfusion/ej2-react-diagrams";
+import SidePanel from './components/SidePanel';
+import SymbolPallete from './components/SymbolPallete';
+
 
 function App() {
+  let snapSettings = {
+    constraints: SnapConstraints.None,
+};
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <SidePanel/>
+      <div className='diagram-container'>
+    <SymbolPallete/>
+    <DiagramComponent id="container" width={'100%'} height={'600px'} snapSettings={snapSettings}>
+      <Inject services={[BpmnDiagrams,Snapping,ConnectorEditing]}/>
+    </DiagramComponent>
+      </div>
     </div>
-  );
+    );
+    
 }
 
 export default App;
---
+++ src/components/SidePanel.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../style/SidePanel.css";
+
+function SidePanel(){
+
+    return(
+        <div className="sidepanel">
+        </div>
+    );
+}
+
+export default SidePanel;
